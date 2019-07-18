1f7f2753f3ff5f718a8518a6173d4f6f1076ad01
Melissa Castro <melissa.anne.castro@gmail.com>
2019-07-18 17:51:02 -0400

    click function works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // 
     // Random number to guess
-    // Assign number to crystals (1-12) dynamic
+    // Assign number to crystals (1-12)
     // Addition on click
     // Count win or lose
     // 
@@ -9,16 +9,21 @@
 
 
 
+$(document).ready(function() {
+
     // Bunch of variables
     var wins = 0
     var losses = 0
     var userScore = 0
 
 
-    // To select random number
+    // Computer select random number
     var randomNumber = Math.floor(Math.random() * 120) + 19
 
     console.log("random number: ", randomNumber)
+
+    // Display random number
+    $("#random-number").html(randomNumber);
 
     // To assign values to each crystal
     var ruby = Math.floor(Math.random() * 12) + 1
@@ -31,50 +36,82 @@
     // Values of crystals on click
     $(".ruby").on("click", function() {
         userScore = userScore + ruby;
-        $("#totalScore").text(userScore); 
+        $("#totalScore").html(userScore); 
+        if (userScore == randomNumber){
+            win();
+          }
+          else if (userScore > randomNumber){
+            lose();
+          }  
+        
+        console.log("New Score = ", userScore)
     })
     
     $(".diamond").on("click", function() {
         userScore = userScore + diamond;
-        $("#totalScore").text(userScore); 
+        $("#totalScore").html(userScore); 
+        if (userScore == randomNumber) {
+            win();
+          }
+          else if (userScore > randomNumber) {
+            lose();
+          }   
     })
 
     $(".sapphire").on("click", function() {
         userScore = userScore + sapphire;
-        $("#totalScore").text(userScore); 
+        $("#totalScore").html(userScore); 
+        if (userScore == randomNumber) {
+            win();
+          }
+          else if (userScore > randomNumber) {
+            lose();
+          }   
     })
 
     $(".emerald").on("click", function() {
         userScore = userScore + emerald;
-        $("#totalScore").text(userScore); 
+        $("#totalScore").html(userScore); 
+        if (userScore == randomNumber) {
+            win();
+          }
+          else if (userScore > randomNumber) {
+            lose();
+          }   
     })
 
 
-    console.log("New Score = ", userScore);
 
-    
+    // Win
+    function win() {
+        wins++;
+        $("#wins").html(wins);
+        reset();
+    }
+
+    // Lose
+    function lose() {
+        losses++;
+        $("#losses").html(losses);
+        reset();
+    }
+
     // Reset
-    function reset(){
+    function reset() {
         randomNumber = Math.floor(Math.random() * 120) + 19;
-        $('#random-number').text(randomNumber);
+        $('#random-number').html(randomNumber);
         ruby = Math.floor(Math.random() * 12) + 1;
         diamond = Math.floor(Math.random() * 12) + 1;
         sapphire = Math.floor(Math.random() * 12) + 1;
         emerald = Math.floor(Math.random() * 12) + 1;
         userScore = 0;
-        $('#totalScore').text(userScore);
-        } 
+        $('#totalScore').html(userScore);
+    } 
 
 
 
 
+    $("#wins").html(wins);
+    $("#losses").html(losses);
 
-
-
-
-
-
-
-    $('#wins').text(wins);
-    $('#losses').text(losses);
-
+})
