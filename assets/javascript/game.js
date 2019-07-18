d521827dfbb960edf6e8d50d4bba8b71262520e9
Melissa Castro <melissa.anne.castro@gmail.com>
2019-07-18 01:36:16 -0400

    added click events for crystals in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,15 @@
 // 
-    // Create number to guess
-    // Assign number attributes to crystals (1-15) dynamic
+    // Random number to guess
+    // Assign number to crystals (1-12) dynamic
     // Addition on click
-    // Alert win or lose
+    // Count win or lose
     // 
     // Crystal pictures
     // 
 
-    // Global variables
+
+
+    // Bunch of variables
     var wins = 0
     var losses = 0
     var userScore = 0
@@ -16,14 +18,63 @@
     // To select random number
     var randomNumber = Math.floor(Math.random() * 120) + 19
 
-    console.log(randomNumber)
+    console.log("random number: ", randomNumber)
 
     // To assign values to each crystal
-    var rubyValue = Math.floor(Math.random() * 12) + 1
-    var diamondValue = Math.floor(Math.random() * 12) + 1
-    var sapphireValue = Math.floor(Math.random() * 12) + 1
-    var emeraldValue = Math.floor(Math.random() * 12) + 1
+    var ruby = Math.floor(Math.random() * 12) + 1
+    var diamond = Math.floor(Math.random() * 12) + 1
+    var sapphire = Math.floor(Math.random() * 12) + 1
+    var emerald = Math.floor(Math.random() * 12) + 1
 
-    console.log(rubyValue)
+    console.log("ruby value: ", ruby)
 
-    // 
+    // Values of crystals on click
+    $(".ruby").on("click", function() {
+        userScore = userScore + ruby;
+        $("#totalScore").text(userScore); 
+    })
+    
+    $(".diamond").on("click", function() {
+        userScore = userScore + diamond;
+        $("#totalScore").text(userScore); 
+    })
+
+    $(".sapphire").on("click", function() {
+        userScore = userScore + sapphire;
+        $("#totalScore").text(userScore); 
+    })
+
+    $(".emerald").on("click", function() {
+        userScore = userScore + emerald;
+        $("#totalScore").text(userScore); 
+    })
+
+
+    console.log("New Score = ", userScore);
+
+    
+    // Reset
+    function reset(){
+        randomNumber = Math.floor(Math.random() * 120) + 19;
+        $('#random-number').text(randomNumber);
+        ruby = Math.floor(Math.random() * 12) + 1;
+        diamond = Math.floor(Math.random() * 12) + 1;
+        sapphire = Math.floor(Math.random() * 12) + 1;
+        emerald = Math.floor(Math.random() * 12) + 1;
+        userScore = 0;
+        $('#totalScore').text(userScore);
+        } 
+
+
+
+
+
+
+
+
+
+
+
+    $('#wins').text(wins);
+    $('#losses').text(losses);
+
